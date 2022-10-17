1a0d32eb137e9f79f1ac60c95ffd6afae3cdbd22
jossuel-js <84153809+jossuel-js@users.noreply.github.com>
2022-10-17 15:46:11 -0300

    Delete data/mongo directory

--- data/mongo/mongodatabase.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-
-
- const aux =  mongoose.connect(
-        'mongodb+srv://jossueljs:3h32gvY0fKIPzlCm@apicluster.9pleccd.mongodb.net/?retryWrites=true&w=majority'
-        )
-
-        module.exports = aux;
